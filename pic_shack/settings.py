a554e980796f00cb0f8559d834e129c3cd906cab
Bertie <albertogheorghita90@gmail.com>
2022-05-10 13:07:48 +0000

    Added home app and templates

--- pic_shack/settings.py
+++ pic_shack/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'home',
 
     # allauth
     'allauth',
@@ -61,7 +62,11 @@ ROOT_URLCONF = 'pic_shack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
