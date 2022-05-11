7723274f29fb4a726d487d4908068f9020712644
Bertie <albertogheorghita90@gmail.com>
2022-05-11 14:18:08 +0000

    Added content to base.html and added static setting to settings.py file

--- pic_shack/settings.py
+++ pic_shack/settings.py
@@ -59,6 +59,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'pic_shack.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -151,6 +154,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
