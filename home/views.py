250e8e12c0ca3386a3ce7b12794454358e5aae20
Bertie <albertogheorghita90@gmail.com>
2022-05-27 10:45:57 +0000

    Added newly added section to index page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def index(request):
     """View to return the index page"""
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
     
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
