b8fdb30855e511d8436432087403fa3fcc1a4d11
Bertie <albertogheorghita90@gmail.com>
2022-06-12 13:26:34 +0000

    Add email host user in env.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -12,29 +12,35 @@ import time
 
 
 class StripeWH_Handler:
-    """ Handle Stripe webhooks """
+    """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
 
     def _send_confirmation_email(self, order):
-        """ Send the user an order confirmation email """
-        customer_email = order.email
+        """Send the user a confirmation email"""
+        cust_email = order.email
         subject = render_to_string(
-            'templates/checkout/confirmation_emails/confirmation_email_subject.txt',
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
         body = render_to_string(
-            'templates/checkout/confirmation_emails/confirmation_email_body.txt',
+            'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [customer_email])
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )        
 
     def handle_event(self, event):
-        """ Handle a generic/unknown/unexpected webhook event """
-
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
-            status=200
-        )
+            status=200)
 
     def handle_payment_intent_succeeded(self, event):
         """
