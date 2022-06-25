10659221e985c59dac720bc1c3fb559520de8eae
Bertie <albertogheorghita90@gmail.com>
2022-06-25 17:11:04 +0000

    Add date created field to product model

--- products/models.py
+++ products/models.py
@@ -30,7 +30,14 @@ class Product(models.Model):
     )
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    created_on = models.DateField(auto_now_add=True)
 
+    class Meta:
+        """Products in descending order"""
+    
+        ordering = ["-created_on"]
+
+    
     def __str__(self):
         return self.name
 
